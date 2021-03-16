65203316cf9a2d2fc056aff761321559163b69a5
joel <joelcrawford30@gmail.com>
2021-03-16 11:31:15 +0300

    first commit

---
+++ HeartRateMonitor/HRMViewController.swift
@@ -0,0 +1,291 @@
+/**
+ * Copyright (c) 2017 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+import CoreBluetooth
+
+class HRMViewController: UIViewController {
+    
+    @IBOutlet weak var heartRateLabel: UILabel!
+    @IBOutlet weak var bodySensorLocationLabel: UILabel!
+    
+    var centralManager: CBCentralManager!
+    
+    let heartRateServiceCBUUID = CBUUID(string: "0x180D") //UUID for the heart sensor
+    
+    
+    let heartRateMeasurementCharacteristicCBUUID = CBUUID(string: "2A37")
+    let bodySensorLocationCharacteristicCBUUID = CBUUID(string: "2A38")
+    
+    
+    //Add a heartRatePeripheral instance variable of type CBPeripheral at the top, to stop scanning for device,,,, we store a reference to our sensor
+    var heartRatePeripheral: CBPeripheral!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Make the digits monospaces to avoid shifting when the numbers change
+        heartRateLabel.font = UIFont.monospacedDigitSystemFont(ofSize: heartRateLabel.font!.pointSize, weight: .regular)
+        
+        
+        //=====Initialize the CBManager=======
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+        
+        //        //=====Scanning for pepherial after turning BT on ======
+        //        centralManager.scanForPeripherals(withServices: nil)
+        
+        
+        
+        
+    }
+    
+    
+    
+    func onHeartRateReceived(_ heartRate: Int) {
+        heartRateLabel.text = String(heartRate)
+        print("BPM: \(heartRate)")
+    }
+    
+    
+    
+    
+    
+    
+}
+
+
+extension HRMViewController: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        
+        
+        //======Handling the  different states========
+        
+        switch central.state {
+        case .unknown:
+            print("central.state is .unknown")
+            
+        case .resetting:
+            print("central.state is .resetting")
+            
+        case .unsupported:
+            print("central.state is .unsupported")
+            
+            
+        case .unauthorized:
+            print("central.state is .unauthorized")
+            
+            
+        case .poweredOn:
+            //             centralManager.scanForPeripherals(withServices: nil)
+            
+            
+            centralManager.scanForPeripherals(withServices: [heartRateServiceCBUUID])
+            print("central.state is .poweredOn")
+            
+            
+            //=====After connecting, we discover the services, and this is only in ON state =====
+            heartRatePeripheral.discoverServices([heartRateServiceCBUUID])
+            
+            
+        case .poweredOff:
+            print("central.state is .poweredOff")
+            
+            
+            
+        }
+    }
+    
+    
+    //=======Discover method (To discover the sensor)======
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        
+        //======Prints a variety of Bluetooth device in vicinity======
+        print(peripheral)
+        
+        //=====Store the reference and stop scanning=====
+        heartRatePeripheral = peripheral
+        
+        
+        //=========Its delegate pointing to self=======
+        heartRatePeripheral.delegate = self
+        
+        
+        
+        centralManager.stopScan()
+        
+        //======We connect to pepherial to obtain data from it ====
+        centralManager.connect(heartRatePeripheral)
+        
+        //        //=====After connecting, we discover the services, and this is only in ON state =====
+        //        heartRatePeripheral.discoverServices(nil)
+        
+        
+        
+        
+    }
+    
+    
+    //=====Delegate to make sure we are really connected =====
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        
+        print("Connected!")
+    }
+    
+    
+}
+
+
+
+
+extension HRMViewController: CBPeripheralDelegate {
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        
+        //=======Returns an array of services ====
+        guard let services = peripheral.services else { return }
+        
+        for service in services {
+            
+            print(service)
+            
+            
+            // print(service.characteristics ?? "characteristics are nil")
+            
+            peripheral.discoverCharacteristics(nil, for: service)
+            
+            
+            
+        }
+        
+    }
+    
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        
+        guard let characteristics = service.characteristics else { return }
+        
+        for characteristic in characteristics {
+            print(characteristic)
+            
+            
+            //=========Each characteristic has properties, that is the read, notify,
+            
+            if characteristic.properties.contains(.read) {
+                print("\(characteristic.uuid): properties contains .read")
+                
+                
+                peripheral.readValue(for: characteristic)
+            }
+            if characteristic.properties.contains(.notify) {
+                print("\(characteristic.uuid): properties contains .notify")
+                
+                //======Receives the heart rate ===
+                peripheral.setNotifyValue(true, for: characteristic)
+            }
+            
+        }
+        
+        
+    }
+    
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        
+        //        switch characteristic.uuid {
+        //          case bodySensorLocationCharacteristicCBUUID:
+        //            print(characteristic.value ?? "no value")
+        //          default:
+        //            print("Unhandled Characteristic UUID: \(characteristic.uuid)")
+        //        }
+        
+        
+        switch characteristic.uuid {
+        case bodySensorLocationCharacteristicCBUUID:
+            let bodySensorLocation = bodyLocation(from: characteristic)
+            bodySensorLocationLabel.text = bodySensorLocation
+            
+          //===== read the heart rate from the characteristic.=======
+        case heartRateMeasurementCharacteristicCBUUID:
+            let bpm = heartRate(from: characteristic)
+            //updates the UI with your heart rate.
+            onHeartRateReceived(bpm)
+            
+        default:
+            print("Unhandled Characteristic UUID: \(characteristic.uuid)")
+        }
+    }
+    
+    
+    
+    //====Helper function =====
+    private func heartRate(from characteristic: CBCharacteristic) -> Int {
+        
+        //From characteristic.value, which is an object of type Data, you create an array of bytes.
+        guard let characteristicData = characteristic.value else { return -1 }
+        let byteArray = [UInt8](characteristicData)
+        
+        let firstBitValue = byteArray[0] & 0x01
+        if firstBitValue == 0 {
+            // Heart Rate Value Format is in the 2nd byte
+            return Int(byteArray[1])
+        } else {
+            // Heart Rate Value Format is in the 2nd and 3rd bytes
+            return (Int(byteArray[1]) << 8) + Int(byteArray[2])
+        }
+    }
+    
+    
+    
+}
+
+
+
+
+
+//The specification shows you that Body Sensor Location is represented by an 8-bit value, so there are 255 possibilities, and only 0 – 6 are used at present. Based on the specification
+
+private func bodyLocation(from characteristic: CBCharacteristic) -> String {
+    guard let characteristicData = characteristic.value,
+        let byte = characteristicData.first else { return "Error" }
+    
+    switch byte {
+    case 0: return "Other"
+    case 1: return "Chest"
+    case 2: return "Wrist"
+    case 3: return "Finger"
+    case 4: return "Hand"
+    case 5: return "Ear Lobe"
+    case 6: return "Foot"
+    default:
+        return "Reserved for future use"
+    }
+}
